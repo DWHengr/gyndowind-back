2991b528f55b14cb0f4539a971dbda5dee442783
Heath <duweihengmr@163.com>
2023-10-31 17:10:38 +0800

    feat: init commit

---
+++ src/main/java/com/dwh/gyndowindback/GyndowindBackApplication.java
@@ -0,0 +1,13 @@
+package com.dwh.gyndowindback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyndowindBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GyndowindBackApplication.class, args);
+	}
+
+}
