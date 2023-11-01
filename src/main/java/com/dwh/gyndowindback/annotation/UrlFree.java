90a69bc7cafce868af2b501a6e3c0e504325bd96
Heath <duweihengmr@163.com>
2023-11-01 17:28:02 +0800

    feat: add UrlPassRunner

---
+++ src/main/java/com/dwh/gyndowindback/annotation/UrlFree.java
@@ -0,0 +1,10 @@
+package com.dwh.gyndowindback.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface UrlFree {
+    String value() default "";
+}
