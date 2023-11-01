eafa81043a86610c746c23c0ff09d92d5c3c8145
Heath <duweihengmr@163.com>
2023-11-01 17:31:56 +0800

    feat: add user table

---
+++ gyndowind.sql
@@ -0,0 +1,17 @@
+DROP TABLE if EXISTS `user`;
+CREATE TABLE `user`
+(
+    `id`            varchar(64)  NOT NULL,
+    `account`       varchar(64)  NOT NULL COMMENT '用户账号',
+    `name`          varchar(200) NOT NULL COMMENT '用户名',
+    `password`      varchar(200) NOT NULL COMMENT '密码',
+    `sex`           varchar(64)  default NULL COMMENT '性别',
+    `phone`         varchar(64)  default NULL COMMENT '手机号',
+    `email`         varchar(200) default NULL COMMENT '邮箱',
+    `portrait`      text         default NULL COMMENT '头像',
+    `status`        varchar(64) COMMENT '用户状态',
+    `nps_client_id` varchar(64) COMMENT 'nps客户端Id',
+    `create_time`   varchar(64)  DEFAULT 0 COMMENT '创建时间',
+    `update_time`   varchar(64)  DEFAULT 0 COMMENT '更新时间',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户表' row_format=dynamic;
